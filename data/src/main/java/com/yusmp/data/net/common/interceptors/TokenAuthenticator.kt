d3d5cea415a8c74cb1c50933a7ee93051055afb2
Сергей Корнюшин <sergey.kornyushin@sequenia.com>
2025-01-28 17:16:04 +0700

    refactored TokenAuthenticator

--- data/src/main/java/com/yusmp/data/net/common/interceptors/TokenAuthenticator.kt
+++ data/src/main/java/com/yusmp/data/net/common/interceptors/TokenAuthenticator.kt
@@ -2,20 +2,17 @@ package com.yusmp.data.net.common.interceptors
 
 import com.yusmp.data.net.auth.model.AuthDataResponse
 import com.yusmp.data.net.auth.model.RefreshTokenRequest
-import com.yusmp.data.net.common.responseCount
-import com.yusmp.data.net.common.interceptors.HeadersInterceptor.Companion.HEADER_AUTH
 import com.yusmp.domain.auth.ClearSessionUseCase
 import com.yusmp.domain.auth.GetCurrentSessionUseCase
 import com.yusmp.domain.auth.UpdateTokensParams
 import com.yusmp.domain.auth.UpdateTokensUseCase
-import com.yusmp.domain.auth.model.SessionType
+import com.yusmp.domain.auth.model.Session
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
-import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
@@ -23,7 +20,6 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.logging.HttpLoggingInterceptor
 import java.util.concurrent.TimeUnit
 
-@Suppress("LongParameterList", "MagicNumber", "ReturnCount")
 class TokenAuthenticator(
     private val appScope: CoroutineScope,
     private val getCurrentSessionUseCase: GetCurrentSessionUseCase,
@@ -35,88 +31,88 @@ class TokenAuthenticator(
 ) : Authenticator {
 
     private val mutex = Mutex()
-    var refreshTokenJob: Job? = null
+    private var refreshTokenJob: Job? = null
+
     override fun authenticate(route: Route?, response: Response): Request? {
         val session = runBlocking { getCurrentSessionUseCase(Unit) }
 
-        if (session.type != SessionType.AUTHORIZED_USER || session.refreshToken.isBlank()) {
-            return null
-        }
+        if (session.isAuthorized().not() || session.refreshToken.isBlank()) return null
 
         runBlocking {
-            mutex.withLock {
-                if (refreshTokenJob == null || refreshTokenJob?.isCompleted == true) {
-                    refreshTokenJob = appScope.launch {
-                        val tokenResponse = refreshToken(RefreshTokenRequest(session.refreshToken))
-                        tokenResponse?.let {
-                            updateTokensUseCase(
-                                UpdateTokensParams(
-                                    it.token.orEmpty(),
-                                    session.refreshToken
-                                )
-                            )
-                        }
-                    }
-                }
-            }
+            mutex.withLock { updateToken(session = session) }
 
-            // Wait for the refresh token job to complete
             refreshTokenJob?.join()
         }
 
-        // Get the updated token
         val updatedSession = runBlocking { getCurrentSessionUseCase(Unit) }
         return response.request.newBuilder()
-            .header("Authorization", "Bearer ${updatedSession.accessToken}")
-            .build()
-    }
-
-    private fun refreshToken(refreshToken: RefreshTokenRequest): AuthDataResponse? {
-        val okHttpClient = createOkHttpClient()
-        val mediaType = "application/json; charset=utf-8".toMediaTypeOrNull()
-        val request = Request.Builder()
-            .url("$apiUrl$REFRESH_PATH")
-            .post(
-                serializer.encodeToString(RefreshTokenRequest.serializer(), refreshToken)
-                    .toRequestBody(mediaType)
+            .header(
+                name = AUTHORIZATION_HEADER_NAME,
+                value = "$AUTHORIZATION_HEADER_PREFIX${updatedSession.accessToken}"
             )
             .build()
-        val response = okHttpClient.newCall(request).execute()
-        return if (response.isSuccessful) {
-            try {
-                val body = response.body?.string()
-                if (body.isNullOrBlank()) null
-                else serializer.decodeFromString(AuthDataResponse.serializer(), body)
-            } catch (e: SerializationException) {
-                null
+    }
+
+    private fun updateToken(session: Session) {
+        if (refreshTokenJob != null && refreshTokenJob?.isCompleted == false) return
+
+        refreshTokenJob = appScope.launch {
+            getRefreshToken(RefreshTokenRequest(session.refreshToken))?.let { token ->
+                updateTokensUseCase(UpdateTokensParams(token, session.refreshToken))
             }
-        } else {
-            appScope.launch {
-                clearSessionUseCase(Unit)
-            }
-            null
         }
     }
 
-    private fun createOkHttpClient(): OkHttpClient { // build new client here to prevent dependency cycle
-        val httpClientBuilder = OkHttpClient.Builder().apply {
-            connectTimeout(15, TimeUnit.SECONDS)
-            readTimeout(60, TimeUnit.SECONDS)
-            writeTimeout(30, TimeUnit.SECONDS)
-            if (isDebugEnvironment) {
-                val logLevel = HttpLoggingInterceptor.Level.BODY
-                val httpLoggingInterceptor = HttpLoggingInterceptor()
-                addInterceptor(
-                    httpLoggingInterceptor.apply {
-                        httpLoggingInterceptor.level = logLevel
-                    }
-                )
+    private fun getRefreshToken(refreshToken: RefreshTokenRequest): String? {
+        val requestBody = serializer.encodeToString(RefreshTokenRequest.serializer(), refreshToken)
+            .toRequestBody(contentType = MEDIA_TYPE_VALUE.toMediaTypeOrNull())
+        val request = Request.Builder()
+            .url("$apiUrl$REFRESH_PATH")
+            .post(requestBody)
+            .build()
+        val response = createOkHttpClient().newCall(request).execute()
+
+        return when (response.isSuccessful) {
+            true -> runCatching {
+                val stringBody = response.body?.string()
+                when (stringBody.isNullOrBlank()) {
+                    true -> null
+                    else -> serializer.decodeFromString(
+                        deserializer = AuthDataResponse.serializer(),
+                        string = stringBody
+                    ).token
+                }
+            }.getOrNull()
+
+            false -> {
+                appScope.launch { clearSessionUseCase(Unit) }
+                null
             }
         }
-        return httpClientBuilder.build()
+    }
+
+    private fun createOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().apply {
+            connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+            readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+            writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
+
+            if (isDebugEnvironment) {
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                    addInterceptor(interceptor = this)
+                }
+            }
+        }.build()
     }
 
     private companion object {
         const val REFRESH_PATH = "/tokenRefresh"
+        const val CONNECT_TIMEOUT = 15L
+        const val READ_TIMEOUT = 60L
+        const val WRITE_TIMEOUT = 30L
+        const val MEDIA_TYPE_VALUE = "application/json; charset=utf-8"
+        const val AUTHORIZATION_HEADER_NAME = "Authorization"
+        const val AUTHORIZATION_HEADER_PREFIX = "Bearer "
     }
 }
