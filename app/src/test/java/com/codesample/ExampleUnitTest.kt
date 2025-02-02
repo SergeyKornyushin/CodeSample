129129dd9209ba932d97ef78188a56953594519d
Сергей Корнюшин <sergey.kornyushin@sequenia.com>
2025-02-02 21:16:14 +0700

    remove default unit test

--- app/src/test/java/com/codesample/ExampleUnitTest.kt
+++ app/src/test/java/com/codesample/ExampleUnitTest.kt
@@ -1,8 +1,5 @@
 package com.codesample
 
-import com.codesample.domain.common.model.ApiError
-import com.codesample.domain.common.model.CommonBackendFailure
-import com.codesample.presentation.common.extentions.getFormattedThrowable
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -15,25 +12,5 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
-    }
-
-    @Test
-    fun test_log_message_format_of_getFormattedThrowable() {
-        val throwable = Throwable("test_log_message_format_of_logToFirebase")
-        println(getFormattedThrowable(customKey = "test_log_message_format_of_logToFirebase", throwable = throwable))
-        println(getFormattedThrowable(customKey = null, throwable = throwable))
-
-        val failure: Throwable = CommonBackendFailure(
-            ApiError(
-                extraMessage = "CommonBackendFailure extraMessage",
-                code = 0,
-                message = "CommonBackendFailure message",
-                violations = emptyList(),
-                key = "CommonBackendFailure key",
-                email = "CommonBackendFailure email"
-            )
-        )
-        println()
-        println(getFormattedThrowable(null, failure))
     }
 }
